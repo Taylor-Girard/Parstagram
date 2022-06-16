7f5bf7f2dfd35a75df4aa9decc197d1800ad3dc1
Taylor Girard <taylorgirard@fb.com>
2022-06-16 15:33:02 -0700

    Splash screen duration increased and details page layout margins added

--- app/src/main/java/com/taylorgirard/parstagram/activities/SplashScreenActivity.java
+++ app/src/main/java/com/taylorgirard/parstagram/activities/SplashScreenActivity.java
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
-        finish();
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
+            finish();
+        }, 750);
     }
 }
